5a80342520eeff7df1e0785e69fee2e8421b3f59
Asus <suyashadhikari99@gmail.com>
2023-09-16 13:45:16 +0545

    Post method for volunteer register created 1:45

--- src/main/java/com/socialwelfareportal/socialwelfareportal/controller/VolunteerController.java
+++ src/main/java/com/socialwelfareportal/socialwelfareportal/controller/VolunteerController.java
@@ -2,14 +2,22 @@ package com.socialwelfareportal.socialwelfareportal.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class VolunteerController {
     //Route that returns volunteer for event form
     @GetMapping("/event/register")
-    public String getRegisterPage(){
+    public String getRegisterPage(Model model){
+        model.addAllAttributes("volunteerDto", new VolunteerDto());
         return "/main/volunteer/register";
+    }
+
+    @PostMapping("/event/register")
+    public String registerVolunteer(){
+        return "redirect:/";
     }
 }
