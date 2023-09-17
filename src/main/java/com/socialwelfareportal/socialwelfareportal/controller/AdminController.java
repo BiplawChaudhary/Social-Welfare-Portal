35d646e6b3d9350f2dfffe225a3539adcb188519
BiplawChaudhary <bsc19108@cab.edu.np>
2023-09-17 12:52:11 +0545

    Added auto adjustment of budget on allocation

--- src/main/java/com/socialwelfareportal/socialwelfareportal/controller/AdminController.java
+++ src/main/java/com/socialwelfareportal/socialwelfareportal/controller/AdminController.java
@@ -1,10 +1,14 @@
 package com.socialwelfareportal.socialwelfareportal.controller;
 
 
+import com.socialwelfareportal.socialwelfareportal.entity.Budget;
+import com.socialwelfareportal.socialwelfareportal.service.BudgetService;
+import com.socialwelfareportal.socialwelfareportal.service.DonationService;
 import com.socialwelfareportal.socialwelfareportal.service.GrantRequestService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.Banner;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,10 +18,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin")
 public class AdminController {
     private final GrantRequestService grantRequestService;
+    private final BudgetService budgetService;
+    private final DonationService donationService;
 
 
     @GetMapping("/home")
-    public String getAdminHomePage(){
+    public String getAdminHomePage(Model model){
+        model.addAttribute("pendingrequest", grantRequestService.getpendingGrantRequest().size());
+        model.addAttribute("totaldonation", donationService.getTotalDonationAmount());
         return "/admin/home";
     }
 
@@ -49,5 +57,27 @@ public class AdminController {
     public String getSearch(@RequestParam("searchKey") String searchKey, Model model){
         model.addAttribute("result", grantRequestService.getSearchResult(searchKey));
         return "/admin/searchresult";
+    }
+
+    /////////// BUDGET
+    @GetMapping("/admin-budget")
+    public String getAdminBudgetPage(Model model){
+        model.addAttribute("allBudgetSector", budgetService.getAllBudgets());
+        model.addAttribute("dto", new Budget());
+        return "/admin/adminbudget";
+    }
+
+
+    @PostMapping("/add-budget")
+    public String saveBudget(Model model, @ModelAttribute Budget budget){
+        budgetService.saveBudget(budget);
+        return "redirect:/admin/admin-budget";
+    }
+
+
+    @GetMapping("/delete-budget/{id}")
+    public String deleteBudfget(@PathVariable Integer id){
+        budgetService.removeBudget(id);
+        return "redirect:/admin/admin-budget";
     }
 }
--- src/main/java/com/socialwelfareportal/socialwelfareportal/repo/BudgetRepo.java
+++ src/main/java/com/socialwelfareportal/socialwelfareportal/repo/BudgetRepo.java
@@ -3,5 +3,8 @@ package com.socialwelfareportal.socialwelfareportal.repo;
 import com.socialwelfareportal.socialwelfareportal.entity.Budget;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.*;
+
 public interface BudgetRepo extends JpaRepository<Budget, Integer> {
+    Budget findBySector(String sector);
 }
