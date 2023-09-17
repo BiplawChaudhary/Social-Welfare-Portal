f5de62fdd5542b12833574084cc0b5fa13f5e1d4
BiplawChaudhary <bsc19108@cab.edu.np>
2023-09-17 06:36:47 +0545

    Added admin pages

---
+++ src/main/java/com/socialwelfareportal/socialwelfareportal/controller/AdminController.java
@@ -0,0 +1,10 @@
+package com.socialwelfareportal.socialwelfareportal.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+}
